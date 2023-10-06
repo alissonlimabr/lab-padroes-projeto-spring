89b53c260c3acc706d748a074ee9b9f953a5cc2a
Alisson Lima <alissonlimabr.github@gmail.com>
2023-10-05 23:15:37 -0500

    Refatora pasta de Repository

--- src/main/java/one/digitalinnovation/gof/repository/ClienteRepository.java
+++ src/main/java/one/digitalinnovation/gof/repository/ClienteRepository.java
@@ -1,9 +1,13 @@
-package one.digitalinnovation.gof.model;
+package one.digitalinnovation.gof.repository;
 
+import one.digitalinnovation.gof.model.Cliente;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+    List<Cliente> findAll();
 
 }
